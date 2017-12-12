e2ec64b60209d2127a71ed57223a5aa044ee8bb6
batu <ba921@nyu.edu>
2017-12-12 10:00:55 -0800

    updated gitignore, models get big

--- unity-environment/Assets/ML-Agents/MyLearners/Steering/Scrıpts/SteeringAgent.cs
+++ unity-environment/Assets/ML-Agents/MyLearners/Steering/Scrıpts/SteeringAgent.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 public class SteeringAgent : Agent {
 
-    public enum RewardStyle { SimpleSparse, SimpleProgress };
-    public enum StateStyle  { Local };
+    public enum RewardStyle { SimpleSparse, SimpleProgress, ProgressAvoidObs };
+    public enum StateStyle  { Local, Raycast };
     public enum MovementStyle { Local };
 
     public bool DebugMode = true;
@@ -51,9 +51,8 @@ public class SteeringAgent : Agent {
     public override void InitializeAgent() {
         initialRotation = transform.rotation;
         initialPosition = transform.position;
+
         car_rb = GetComponent<Rigidbody>();
-        init_distance = Vector3.Distance(transform.position, Target.transform.position);
-        last_distance = init_distance;
         academy = GameObject.FindGameObjectWithTag("Academy").GetComponent<SteeringAcademy>();
 
         obstacle_list = new List<GameObject>();
@@ -63,15 +62,13 @@ public class SteeringAgent : Agent {
 
     
     void createObstacles() {
-        print("I am in obstacle creation.");
 
         foreach( GameObject obs in obstacle_list) {
             Destroy(obs);
         }
 
-        float z_offset = 5 + academy.target_position_z;
+        float z_offset = 15 + academy.target_position_z;
         float x_offset = 8;
-        print(academy.num_obstacles);
         for (int i = 0; i <= academy.num_obstacles; i++){
             GameObject obs = Instantiate(obstacle, initialPosition + new Vector3(Random.Range(-x_offset, x_offset), 0f, Random.Range(5, z_offset)), Quaternion.identity, obstacle_holder.transform) as GameObject;
             obstacle_list.Add(obs);
@@ -87,9 +84,6 @@ public class SteeringAgent : Agent {
 
         List<float> state = new List<float>();
 
-        state.Add(transform.position.x);
-        state.Add(transform.position.z);
-
         state.Add(transform.rotation.eulerAngles.y / 180.0f - 1.0f);
 
         state.Add(car_rb.velocity.x);
@@ -97,40 +91,50 @@ public class SteeringAgent : Agent {
 
         state.Add(car_rb.angularVelocity.y);
 
+        if (DebugMode) Debug.DrawLine(transform.position, target.transform.position);
+        state.Add(Vector3.Distance(transform.position, target.transform.position));
+        state.Add(target.transform.position.x - transform.position.x);
+        state.Add(target.transform.position.z - transform.position.z);
 
-        Vector3[] target_points = FindClosestTargetPoints("Target", 1);
-        foreach (Vector3 target_point in target_points) {
-            if (DebugMode) Debug.DrawLine(transform.position, target_point);
-            state.Add(target_point.x - transform.position.x);
-            state.Add(target_point.z - transform.position.z);
-        }
 
-        Vector3[]  obs_points = FindClosestTargetPoints("Obstacle", 5);
+        /*
+        Vector3[]  obs_points = FindClosestTargetPoints("Obstacle", 3);
         foreach(Vector3 obs_point in obs_points) {
             if(DebugMode) Debug.DrawLine(transform.position, obs_point);
+            state.Add(Vector3.Distance(transform.position, obs_point));
             state.Add(obs_point.x - transform.position.x);
             state.Add(obs_point.z - transform.position.z);
         }
+        */
 
-        Vector3[]  wall_points = FindClosestTargetPoints("Wall", 2);
+        Vector3[]  wall_points = FindClosestTargetPoints("Wall", 5);
         foreach (Vector3 wall_point in wall_points) {
             if (DebugMode) Debug.DrawLine(transform.position, wall_point);
+            state.Add(Vector3.Distance(transform.position, wall_point));
             state.Add(wall_point.x - transform.position.x);
             state.Add(wall_point.z - transform.position.z);
         }
 
         cumulReward += reward;
-        //print(cumulReward);
         return state;
     }
 
 
     private void OnCollisionEnter(Collision collision) {
         if (collision.collider.gameObject.tag == "Target" && hitting_target_rewards_one) {
-            reward += 1;
-            cumulReward = -1;
+            reward += 10;
+            cumulReward -= 10;
             done = true;
+
             //Debug.Log(string.Format("OnCollisionEnter cummulative reward is:{0}", CumulativeReward));
+        }
+        if(rewardStyle == RewardStyle.ProgressAvoidObs) {
+            if (collision.collider.gameObject.tag == "Wall" || collision.collider.gameObject.tag == "Obstacle") {
+                reward -= 1f;
+                cumulReward = +1;
+                done = true;
+                //Debug.Log(string.Format("OnCollisionEnter cummulative reward is:{0}", CumulativeReward));
+            }
         }
     }
 
@@ -147,15 +151,20 @@ public class SteeringAgent : Agent {
             car_rb.AddForce(transform.forward * speed_mult, ForceMode.VelocityChange);
         }
         if (action == 1) {
-            car_rb.AddForce(-transform.forward * speed_mult, ForceMode.VelocityChange);
-        }
-        if (action == 2) {
             car_rb.AddTorque(0f, turn_mult, 0f, ForceMode.VelocityChange);
         }
-        if (action == 3) {
+        if (action == 2) {
             car_rb.AddTorque(0f, -turn_mult, 0f, ForceMode.VelocityChange);
         }
-
+        if (action == 3) {
+           //Noop
+        }
+        car_rb.velocity = car_rb.velocity.magnitude * transform.forward;
+        /*
+        if (action == 1) {
+            car_rb.AddForce(-transform.forward * speed_mult, ForceMode.VelocityChange);
+        }
+        */
         if (car_rb.velocity.magnitude > maxSpeed) car_rb.velocity = car_rb.velocity * slowing_down_constant;
 
 
@@ -174,19 +183,36 @@ public class SteeringAgent : Agent {
 
                 reward += delta / 10f;
                 reward -= MinusRewardStep;
-
+            
                 break;
+            case RewardStyle.ProgressAvoidObs:
+                hitting_target_rewards_one = true;
+
+                this_distance = Vector3.Distance(transform.position, Target.transform.position);
+                delta = last_distance - this_distance;
+                last_distance = this_distance;
+
+                reward += delta / 10f;
+                reward -= MinusRewardStep;
+                break;
+
         }
 
     }
+
 
 	public override void AgentReset()
 	{
         //Debug.Log(string.Format("Agent reset called in SteeringAgent.cs and the cummulative reward is:{0}", CumulativeReward));
         createObstacles();
+        cumulReward = 0;
 
-        transform.position = initialPosition;
-        transform.rotation = initialRotation;
+        transform.position = initialPosition + new Vector3(Random.Range(-8, 8), 0f, Random.Range(-3,3));
+        transform.rotation = Quaternion.identity;
+        //transform.rotation = Quaternion.Euler(0f,Random.Range(0,360), 0f);
+        init_distance = Vector3.Distance(transform.position, Target.transform.position);
+        last_distance = init_distance;
+        
 
         last_distance = init_distance;
 
@@ -194,7 +220,7 @@ public class SteeringAgent : Agent {
         car_rb.angularVelocity = Vector3.zero;
 
        
-        target.transform.position = init_target_pos + new Vector3(0f, 0f, academy.target_position_z);
+        target.transform.position = init_target_pos + new Vector3(0f, 0f, Random.Range(academy.target_position_z, academy.target_position_z + 5));
 
    
     }
